0113a445732541f7218da728444325d686a0c90d
Divyanshu Kushwaha <divyanshu4381@gmail.com>
2025-07-04 10:35:10 +0530

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -167,7 +167,8 @@ export const userLogin = asyncHandler(async (req, res) => {
     return res.status(200)
         .cookie("accessToken", accessToken, options)
         .cookie("refreshToken", refreshToken, options)
-        .json(new ApiResponse(200, { user: loggedInUser }, "Customer Login Successful"));
+        .json(new ApiResponse(200, { user: loggedInUser,accessToken,
+        refreshToken  }, "Customer Login Successful"));
 
 })
 
